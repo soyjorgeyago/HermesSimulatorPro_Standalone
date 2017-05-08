7c6089e3273c0fee5cac9682c0177edcd9c9294f
Alvaro Arcos <arcos1989@gmail.com>
2017-05-08 11:30:53 +0200

    Post merge cleanup

--- src/main/java/es/us/lsi/hermes/Main.java
+++ src/main/java/es/us/lsi/hermes/Main.java
@@ -1,7 +1,6 @@
 package es.us.lsi.hermes;
 
 import es.us.lsi.hermes.simulator.SimulatorController;
-import es.us.lsi.hermes.util.NetworkMonitor;
 import org.apache.commons.lang.SystemUtils;
 
 import javax.swing.*;
@@ -13,19 +12,7 @@ public class Main extends JFrame {
      */
     public static void main(String[] args) {
 
-        if(SystemUtils.IS_OS_LINUX)
-            new Main();
-
         SimulatorController sc = new SimulatorController();
         sc.simulate();
-    }
-
-    // Network Monitor interface
-    private Main() {
-        setSize(550, 200);
-        setTitle("Network Monitor");
-        setContentPane(new NetworkMonitor());
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setVisible(true);
     }
 }
